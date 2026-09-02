d42bf379b4e2370b395391a5bcae36c19d8d7194
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-02 19:52:45 +0800

    day001.java
    Struktur Java dan semua jenis komen (pertambahan pada variabel)

---
+++ day001.java
@@ -0,0 +1,11 @@
+public class day1 {
+    public static void main(String[] args) {
+        //ayam bernilai/sebanyak 3
+        //kucing bernilai/sebanyak 4
+        int ayam = 3;
+        int kucing = 4;
+        int total = ayam + kucing;
+        /*menampilkan hasil perjumlahan ayam + kucing*/ 
+        System.out.println("Total :" + total);
+    }
+}
