0ae451579649017f6891deb538791ceb2e22b258
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-03 09:44:25 +0800

    day002.java
    Membuat Biodata diri dengan menggunakan println(),printf(),print()

---
+++ day2.java
@@ -0,0 +1,9 @@
+public class day2 {
+    public static void main(String[] args) {
+        System.out.println("Nama : AdliGanteng");
+        System.out.println("Cewek : tidak ada");
+        System.out.println("Grup : 2");
+        System.out.printf("Umur : 18 \n");
+    }
+
+}
