46663fe364bbdb1e1b3ae57e2187345167a0b468
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-03 21:06:27 +0800

    day002.java
    Membuat Biodata diri dengan Variabel nama,umur,tinggi menggunakan print(),println(), dan printf()
    
    fungsi print():
    membuat/mencetak output dengan hanya 1 baris saja
    println():
    Sama seperti print(), cuman println membuat garis baru di bawahnya agar  tidak bertabrakan pada variabel lainnya pada output
    printf():
    Sama seperti print() dan print(), namun printf harus menggunakan format tertentu

--- day002.java
+++ day002.java
@@ -1,9 +1,26 @@
 public class day2 {
     public static void main(String[] args) {
-        System.out.printf("Nama : AdliGanteng \n");
-        System.out.println("Cewek : tidak ada");
-        System.out.println("Grup : 2");
-        System.out.print("Umur : 18");
+        // Variabel adalah sekumpulan data yg akan di proses untuk menghasilkan output
+        // yg diinginkan
+        String nama = "M.Adli Syazwi";
+        String cewek = "ada lah";
+        int umur = 18;
+        int tinggi = 178;
+        int grup = 2;
+
+        // println() untuk menunjukkan/menampilkan Judul biodata lalu menmbuat garis
+        // baru dibawahnya agar outputnya tidak bersambung dengan variabel lain
+        System.out.println("========= BIODATA DIRI =========");
+        // printf() itu sama dengan println, cuman bedanya printf menggunakan format
+        // tertentu untuk menghasilkan output
+        System.out.printf("%-15s : %s%n", "Nama", nama);
+        System.out.println("Umur            : " + umur);
+        System.out.println("Tinggi          : " + tinggi);
+        System.out.println("Grup            : " + grup);
+        // print() sama dengan println dan printf, bedanya hanya di penempatan output yg
+        // dimana hanya di satu baris saja tanpa membuat garis baru
+        System.out.print("Ada cewek?      : " + cewek);
+
     }
 
 }
