8149e815b398d2b1484450457a32c992adf4559a
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-04 22:36:45 +0800

    day003.java
    Menggunakan semua Escape Sequence
    // \t (Insert Tab) memberi garis baru pada paragrafnya
     // \n (Newline) membuat garis baru
    // \" (Double quote) memberi tanda (") pada teks
    // \' (Single quote) memberi tanda (') pada teks
    // \\ (Backslash) menampilkan Backslash pada teks
    // \f (Form feed) membuat halaman/paragraf baru pada teks
    // \r (Carriage Return) menimpa teks sebelumnya
    // \b (Backspace) menghapus 1 karakter sebelumnya

---
+++ day003.java
@@ -0,0 +1,32 @@
+public class day003 {
+        public static void main(String[] args) throws InterruptedException {
+                System.out.print("Wait...\n");
+                Thread.sleep(1500);
+                System.out.print("\r3");
+                Thread.sleep(1000);
+                System.out.print("\r2");
+                Thread.sleep(1000);
+                System.out.print("\r1");
+                Thread.sleep(1000);
+                System.out.println("\rSelamat membaca!");
+                Thread.sleep(1000);
+                System.out.println("                                             =========KAMU KUAT=========\n");
+                System.out.println(
+                                "\tDi balik senyum tegar Maya, ada perjuangan sunyi menahan rasa sakit di tubuhnya yang sering menguras tenaga. Di saat-saat paling berat itu, bayangan dan pelukan hangat Oma selalu hadir menenangkan jiwanya, membisikkan kembali nasihat bijak yang tak pernah d\bia lupa.\n");
+                // Paragraf 2
+                System.out.println(
+                                "\t\"\\Rasa sakit tidak pernah menentukan batas kemampuanmu, Nak. Kamuuu diciptakan tangguh, bukan karena tak pernah rapuh, melainkan karena selalu memilih untuk bangkit.\\\"\f");
+                // paragraf 3
+                System.out.println(
+                                "\tMengingat pesan itu, Maya mengusap air matanya dan menghela napas dalam. Rasa sakit boleh saja singgah, tetapi keberanian untuk bertahan tak akan pernah padam. Gadis hebat itu pun berbisik pada dirinya sendiri dengan penuh keyakinan, \'Kamu kuat, Maya. Kamu pasti bisa melewati ini.\'\n");
+                // \t (Insert Tab) memberi garis baru pada paragrafnya
+                // \n (Newline) membuat garis baru
+                // \" (Double quote) memberi tanda (") pada teks
+                // \' (Single quote) memberi tanda (') pada teks
+                // \\ (Backslash) menampilkan Backslash pada teks
+                // \f (Form feed) membuat halaman/paragraf baru pada teks
+                // \r (Carriage Return) menimpa teks sebelumnya
+                // \b (Backspace) menghapus 1 karakter sebelumnya
+
+        }
+}
