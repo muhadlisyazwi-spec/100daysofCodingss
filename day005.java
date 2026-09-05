0075e7772a6a3bda4bccc2c4d84ce345183e6dee
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-06 00:11:10 +0800

    day005.java
    Mereka adalah tipe data bilangan desimal dengan ukuran dan tingkat ketelitian masing masing
    
    // float(single precision) Tipe data desimal dengan ukuran memori 32-bit (4 byte) dengan tingkat ketelitiannya hingga 6-7 digit di belakang koma
    // double (Double Precision) Tipe data desimal dengan ukuran memori 64-bit (8 byte) dengan tingkat ketelitian ganda hingga 15-17 digit di belakang koma

---
+++ day005.java
@@ -0,0 +1,16 @@
+package src;
+
+public class day005 {
+    public static void main(String[] args) {
+        // float(single precision) Tipe data desimal dengan ukuran memori 32-bit (4
+        // byte) dengan tingkat ketelitiannya hingga 6-7 digit di belakang koma
+        float rateMuka = 0.1f;
+        // double (Double Precision) Tipe data desimal dengan ukuran memori 64-bit (8
+        // byte) dengan tingkat ketelitian ganda hingga 15-17 digit di belakang koma
+        double isiRekening = 230300.5;
+
+        System.out.println("Rating muka \t:" + rateMuka);
+        System.out.println("Rating muka \t:" + isiRekening);
+    }
+
+}
