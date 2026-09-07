c30e112a4209c7942f53556e6f5bd58324861002
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-07 22:46:44 +0800

    day007.java
    Pengenalan tipe data String
    
     //String merupakan tipe data yang dapat menampung semua jenis kata,angka,simbol, dan bahkan emoji dengan menggunakan ("")

---
+++ day007.java
@@ -0,0 +1,17 @@
+package src;
+
+public class day007 {
+    public static void main(String[] args) throws InterruptedException {
+        //String merupakan tipe data yang dapat menampung semua jenis kata,angka,simbol, dan bahkan emoji dengan menggunakan ("")
+        String kataKata = "Gak sombong";
+        String kataKata2 = "Gak barbar";
+        String kataKata3 = "UNSULBARRRR";
+
+        System.out.println(kataKata);
+        Thread.sleep(800);
+        System.out.println(kataKata2);
+        Thread.sleep(800);
+        System.out.println(kataKata3);
+        Thread.sleep(700);
+    }
+}
