f952e3b9218fb752ec6fc5ecebdd57fbf85441a4
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-09 15:33:02 +0800

    day008.java
    //Deklarasi adalah proses memperkenalkan/mendaftarkan nama variabel dan nilainya ke java, contohnya nama variabel nilai1 didaftar ke int yg artinya nilainya bilangan bulat
     //Inisialisasi adalah proses pemberi nilai pertama kepada variabel yg sudah terdaftar, contohnya nilai1 diberi nilai 912873098
     //Update variabel adalah hasil perubahan dari nilai pertama ke nilai yg baru dengan menimpa nilai lama, contohnya nilai1 = 912873098 diubah ke nilai1 = 999999999; (nah inilah update variabelnya)

--- day008.java
+++ day008.java
@@ -3,13 +3,13 @@ package src;
 public class day008 {
     public static void main(String[] args) throws InterruptedException {
         //Deklarasi adalah proses memperkenalkan/mendaftarkan nama variabel dan nilainya ke java, contohnya nama variabel nilai1 didaftar ke int yg artinya nilainya bilangan bulat
-        //Inisialisasi adalah proses pemberi nilai pertama kepada variabel yg sudah terdaftar, contohnya nilai1 diberi nilai 1287309
-        int nilai1 = 1287309;
-        int nilai2 = 8273;
-        //Update variabel adalah hasil perubahan dari nilai pertama ke nilai yg baru dengan menimpa nilai lama, contohnya 1287309(Nilai1) x 8273(Nilai2) = 2059972765 (nah inilah update variabelnya)
-        int hasil = (nilai1 * nilai2);
+        int nilai1;
+        //Inisialisasi adalah proses pemberi nilai pertama kepada variabel yg sudah terdaftar, contohnya nilai1 diberi nilai 912873098
+       nilai1 = 912873098;
+        //Update variabel adalah hasil perubahan dari nilai pertama ke nilai yg baru dengan menimpa nilai lama, contohnya nilai1 = 912873098 diubah ke nilai1 = 999999999; (nah inilah update variabelnya)
+        nilai1 = 999999999;
 
-        System.out.println("1287309 x 8273 = " + hasil);
+        System.out.println(nilai1);
         Thread.sleep(2000);
         System.out.println("Hasilnya banyak dan bisa diukur oleh java");
         Thread.sleep(2000);
