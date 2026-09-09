344be0574e8d38a31eefe9e56d69029d5222ba0e
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-09 22:02:12 +0800

    day009.java
    Mengenal constanta
    constanta adalah nilai tetap menggunakan perintah "final" yang tidak bisa diubah ubah meskipun telah masuk ke proses aritmatika
    
    contoh:
    final int = 12345;
    Nilai ini menetap dan tidak bisa diubah

---
+++ day009.java
@@ -0,0 +1,10 @@
+package src;
+
+public class day009 {
+    public static void main(String[] args) {
+        //constanta adalah nilai tetap menggunakan perintah "final" yang tidak bisa diubah ubah meskipun masuk ke proses aritmatika
+        final int nilai1 = 12345;
+        
+    }
+    
+}
