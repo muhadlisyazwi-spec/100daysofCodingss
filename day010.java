c58328642cc5c1f657b3955841b907d3ab604ae7
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-11 09:08:42 +0800

    day010.java
    Pemahaman Batas minimal dan maksimal dari tipe data

---
+++ day010.java
@@ -0,0 +1,14 @@
+package src;
+
+public class day010 {
+    public static void main(String[] args) {
+        System.out.println("byte \t: " + Byte.MIN_VALUE + " s/d " + Byte.MAX_VALUE);
+        System.out.println("short \t: " + Short.MIN_VALUE + " s/d " + Short.MAX_VALUE);
+        System.out.println("int \t: " + Integer.MIN_VALUE + " s/d " + Integer.MAX_VALUE);
+        System.out.println("long \t: " + Long.MIN_VALUE + " s/d " + Long.MAX_VALUE);
+        System.out.println("float \t: " + Float.MIN_VALUE + " s/d " + Float.MAX_VALUE);
+        System.out.println("double \t: " + Double.MIN_VALUE + " s/d " + Double.MAX_VALUE);
+        System.out.println("char \t: " + (int) Character.MIN_VALUE + " s/d " + (int) Character.MAX_VALUE);
+        System.out.println("boolean \t: " + Boolean.MIN_VALUE + " s/d " + Boolean.MAX_VALUE);
+    }
+}
