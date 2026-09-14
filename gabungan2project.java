908f13161fbe1ac9cd692831b69f9707709757ea
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-14 22:59:29 +0800

    day013.java
    gabungan 2 contoh projek menjadi 1

---
+++ gabungan2project.java
@@ -0,0 +1,24 @@
+public class evaluasi2 {
+    public static void main(String[] args) {
+      //projek1
+        System.out.println("Nama    : M.Adli Syazwi ");
+        System.out.print("Hobi  : Main game");
+        System.out.printf("%nUmur     : 18 tahun ");
+    }
+}
+    
+public class eval3 {
+    public static void main(String[] args) {
+      //projek 2
+        String nama = "Andi";
+        char kelas = 'A';
+        String status = "Mahasiswa";
+
+        System.out.print("   BIODATA MAHASISWA\n");
+        System.out.println("--------------------");
+        System.out.println("Nama \t:" + nama);
+        System.out.println("Kelas \t:" + kelas);
+        System.out.println("Status \t:" + status);
+
+    }
+}
