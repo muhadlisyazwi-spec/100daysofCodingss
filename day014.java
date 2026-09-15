157792e0b7cdaf59cc8718b8de7b411a89a2960c
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-15 19:41:48 +0800

    day014.java
    Operasi aritmatika (Penjumlahan dan Pengurangan)

---
+++ day014.java
@@ -0,0 +1,17 @@
+package src;
+
+public class day014 {
+    public static void main(String[] args) {
+        int nilai1 = 20;
+        int nilai2 = 35;
+        int nilai3 = 30;
+        int hasil = (nilai1 + nilai2);
+        System.out.println("20 + 35 ");
+        System.out.println("Hasil penjumlahan : " + hasil);
+
+        final int akhir = (hasil - nilai3);
+        System.out.println("55 - 30");
+        System.out.println("Hasil pengurangan : " + akhir);
+    }
+
+}
