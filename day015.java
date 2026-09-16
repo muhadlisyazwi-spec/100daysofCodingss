71a15729c554c797d581fc5c78b0c06f28f4b0ec
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-16 09:09:09 +0800

    day015.java
    Operator Aritmatika (Perkalian dan Pembagian)

---
+++ day015.java
@@ -0,0 +1,18 @@
+package src;
+
+public class day015 {
+    public static void main(String[] args) {
+
+        double nilai1 = 2500;
+        double nilai2 = 376;
+        int nilai3 = 5;
+        double hasil = (nilai1 * nilai2);
+        System.out.println("25000 X 3762 ");
+        System.out.println("Hasil perkalian : Rp." + hasil);
+
+        final double akhir = (hasil / nilai3);
+        System.out.println("94000 / 30");
+        System.out.println("Hasil pengurangan : Rp." + akhir);
+    }
+
+}
