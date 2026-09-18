1bc1af3e0f8823d2b60bdd2f05fb9235bbea203f
muhadlisyazwi-spec <muh.adli.syazwi@gmail.com>
2026-09-18 17:43:15 +0800

    day017.java
    Operator penugasan

---
+++ day017.java
@@ -0,0 +1,21 @@
+package src;
+
+public class day017 {
+    public static void main(String[] args) {
+        int nilai = 10;
+        nilai += 15;
+        System.out.println(nilai);
+
+        nilai -= 10;
+        System.out.println(nilai);
+
+        nilai *= 5;
+        System.out.println(nilai);
+
+        nilai /= 3;
+        System.out.println(nilai);
+
+        nilai %= 2;
+        System.out.println(nilai);
+    }
+}
